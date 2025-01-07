672534e2291ef2d21227073433761511d8c84671
bharath <pedambharathkumar22@gmail.com>
2025-01-08 01:28:07 +0530

    All the files

---
+++ src/Jan/ex_040125/Lab001.java
@@ -0,0 +1,8 @@
+package Jan.ex_040125;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Java for Automation Testing");
+
+    }
+}
---
+++ src/Task/Day1/T3.java
@@ -0,0 +1,8 @@
+package Task.Day1;
+
+public class T3 {
+    public static void main(String[] args) {
+        System.out.println("Git Learning" );
+    }
+}
+
