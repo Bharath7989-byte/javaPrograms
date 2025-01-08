a800b1d1b8a2ff86eccaa45d96e6fa7224b34d2c
bharath <pedambharathkumar22@gmail.com>
2025-01-08 22:38:18 +0530

    All the files

--- src/Task/Day1/T3.java
+++ src/Task/Day1/T3.java
@@ -2,7 +2,11 @@ package Task.Day1;
 
 public class T3 {
     public static void main(String[] args) {
-        System.out.println("Git Learning" );
+        System.out.println("Git Learning");
+        String name="Learning";
+        for(int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+
     }
 }
-
